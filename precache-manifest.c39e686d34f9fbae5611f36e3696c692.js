9e66cdf62e15767066fdb5e6ee00b9f195d0f0b5
SaravanakumarVijay <saravanakumar.tsm@gmail.com>
2022-04-22 08:54:33 +0530

    Updates

---
+++ precache-manifest.c39e686d34f9fbae5611f36e3696c692.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800655eee2d667bc43215dc1fa57a9aa",
+    "url": "/SaravanakumarVijay/SaraClicks/index.html"
+  },
+  {
+    "revision": "718d9ce4593c81280e8b",
+    "url": "/SaravanakumarVijay/SaraClicks/static/css/2.6d7491e6.chunk.css"
+  },
+  {
+    "revision": "f8a5b9c0a63e1507160a",
+    "url": "/SaravanakumarVijay/SaraClicks/static/css/main.5a345892.chunk.css"
+  },
+  {
+    "revision": "718d9ce4593c81280e8b",
+    "url": "/SaravanakumarVijay/SaraClicks/static/js/2.94a4c4e1.chunk.js"
+  },
+  {
+    "revision": "1f3630a1c12471a3ad24c2f765cd95d3",
+    "url": "/SaravanakumarVijay/SaraClicks/static/js/2.94a4c4e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8a5b9c0a63e1507160a",
+    "url": "/SaravanakumarVijay/SaraClicks/static/js/main.5b29a423.chunk.js"
+  },
+  {
+    "revision": "721c539cdd2412b61ee4",
+    "url": "/SaravanakumarVijay/SaraClicks/static/js/runtime-main.1c5392b2.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/SaravanakumarVijay/SaraClicks/static/media/default-skin.b257fa9c.svg"
+  }
+]);
